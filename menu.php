fb0682e5adb11e357170a7db962a18eab8ba25ed
Bao Uyen <129201109+nbuyen@users.noreply.github.com>
2024-04-03 15:01:46 +0700

    Update code

--- menu.php
+++ menu.php
@@ -10,7 +10,7 @@
             <nav class = "container">
                 <ul id="main-menu" >
                     <li><a href="index.php">Trang chủ</a></li>
-                    <li><a href="">Sản phẩm</a>
+                    <li><a href="index-product.php">Sản phẩm</a>
                     <ul class="sub-menu">
                         <li><a href="index-product.php?category=Lắc">Lắc</a>
                             <ul class="sub-menu">
