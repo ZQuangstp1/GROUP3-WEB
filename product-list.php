4420cc892c7a106dc1ca894ccc160f087cbb74e6
ZQuangstp1 <quangquach.31211026781@st.ueh.edu.vn>
2024-04-04 09:30:03 +0700

    add more margin left to menu

--- product-list.php
+++ product-list.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="product-list.css" rel="stylesheet">]
+    <link href="product-list.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script>
