42c9f666ceda474c29bee7b969b11f268e6c12e1
sunday576 <156815133+sunday576@users.noreply.github.com>
2024-04-12 17:00:37 +0700

    Add files via upload

---
+++ aboutUs.php
@@ -0,0 +1,43 @@
+<!DOCTYPE <html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <title>About Us - Công Ty Trang Sức XYZ</title>
+    <link rel="stylesheet" href="abUs.css">
+</head>
+
+<body>
+    <div class="about-section">
+        <div class="image-overlay">
+            <img src="https://jewelhouse.in/wp-content/uploads/revslider/jewelry2/jewelry-slide-bg-2.jpg"
+                alt="Đội Ngũ XYZ">
+            <div class="overlay-text">
+                <h1 style="color: white">Về Chúng Tôi</h1>
+                <p style="color: white">
+                    Flamingo được thành lập từ năm 2000, chúng tôi tự hào cung cấp các sản phẩm trang sức cao cấp được
+                    làm thủ công với chất lượng và thiết kế tuyệt vời. Sứ mệnh của chúng tôi là tạo ra
+                    những trải nghiệm độc đáo và phong cách cá nhân hóa cho khách hàng, bằng cách mang lại vẻ đẹp
+                    và chất lượng trong từng sản phẩm trang sức mà chúng tôi tạo ra.
+                </p>
+            </div>
+        </div>
+        <div class="image-overlay">
+            <img src="https://img.freepik.com/free-photo/white-wedding-arrangement-with-copy-space_23-2148243968.jpg?t=st=1712915769~exp=1712919369~hmac=f0f6c3fcebb7f1e8f7c9a4fe7b3be95a25a4726b668d2f845945c53ebe68f97d&w=1380"
+                alt="Lịch Sử Công Ty">
+            <div class="overlay-text">
+                <h1>Lịch Sử Hình Thành</h1>
+                <p>Lịch sử hình thành của Flamingo bắt đầu từ ý tưởng của một nhóm các nhà thiết kế và thợ kim hoàn tâm
+                    huyết,
+                    mong muốn chế tác ra những sản phẩm không chỉ tôn vinh vẻ đẹp truyền thống mà còn phá vỡ giới hạn
+                    sáng tạo trong ngành trang sức.
+                    Từ những ngày đầu chỉ là một xưởng nhỏ, Flamingo đã phát triển mạnh mẽ, trở thành một thương hiệu
+                    được yêu thích,
+                    với các sản phẩm được phân phối rộng rãi khắp thế giới.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
