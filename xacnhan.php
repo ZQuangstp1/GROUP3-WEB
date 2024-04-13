14153e55c78bfa1f0e9a03c8f59e74b8848d1afe
Bao Uyen <129201109+nbuyen@users.noreply.github.com>
2024-04-13 09:59:50 +0700

    Update font cho trang của Tú

--- xacnhan.php
+++ xacnhan.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -32,6 +33,10 @@ if (isset($_SESSION['cart'])) {
         <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 
         <link rel="stylesheet" href="xacnhan.css">
         <script type="text/javascript" src="xacnhan.js" language="javascript"></script>
@@ -161,6 +166,7 @@ if (isset($_SESSION['cart'])) {
 
                                 <?php
                                 foreach ($_SESSION['cart'] as $sp) {
+                                    $tong = 0;
                                     $thanhtien = $sp[4] * $sp[3];
                                     $tong += $thanhtien;
                                     echo '  <div class="col-trais" style="margin-top: 3%;">
